ff1f2122de05377c899d15c29199bf2d65b15144
MatthewVaccaro <matthew.tylor.vaccaro@gmail.com>
2021-05-04 18:21:02 -0500

    updated the washroutes and calls in Insomnia

--- routes/washRoute.js
+++ routes/washRoute.js
@@ -1,28 +1,37 @@
-const router = require('express').Router();
-const POST_scrape = require('../controllers/POST_scrape')
-const GET_all = require('../controllers/GET_all');
-const GET_singleDirty = require('../controllers/GET_singleDirty');
-const GET_allTags = require('../controllers/GET_allTags');
-const POST_createTag = require('../controllers/POST_createTag');
-const DEL_singleDirty = require('../controllers/DEL_singleDirty')
-const PUT_singleDirty = require('../controllers/PUT_singleDirty');
-const PUT_cleanToggle = require('../controllers/PUT_cleanToggle');
-require('dotenv').config();
+const router = require("express").Router();
+const POST_scrape = require("../controllers/POST_scrape");
+const GET_all = require("../controllers/GET_all");
+const GET_singleDirty = require("../controllers/GET_singleDirty");
+const GET_allTags = require("../controllers/GET_allTags");
+const POST_createTag = require("../controllers/POST_createTag");
+const DEL_singleDirty = require("../controllers/DEL_singleDirty");
+const PUT_singleDirty = require("../controllers/PUT_singleDirty");
+const PUT_cleanToggle = require("../controllers/PUT_cleanToggle");
+require("dotenv").config();
 
-router.post('/addScrape', POST_scrape());
+// ANCHOR: Base Route: /api/internal/washer
 
-router.get('/all/:washStatus', GET_all());
+router.post("/addRestaurant", POST_scrape());
+// router.post('/addScrape', POST_scrape());
 
-router.get('/singleEntry/:id', GET_singleDirty())
+router.get("/allRestaurants/:washStatus", GET_all());
+// router.get('/all/:washStatus', GET_all());
 
-router.post('/createTag', POST_createTag())
+router.get("/singleRestaurant/:id", GET_singleDirty()); // NOTE: Update to be GET_singleRestaurant
+// router.get('/singleEntry/:id', GET_singleDirty())
 
-router.get('/allTags', GET_allTags())
+router.delete("/singleRestaurant/:id", DEL_singleDirty());
+// router.delete('/singleDirty/:id', DEL_singleDirty())
 
-router.delete('/singleDirty/:id', DEL_singleDirty())
+router.put("/singleRestaurant/:id", PUT_singleDirty());
+// router.put('/singleDirty/:id', PUT_singleDirty())
 
-router.put('/singleDirty/:id', PUT_singleDirty())
+router.put("/washStatus/:id", PUT_cleanToggle());
+// router.put('/cleanToggle/:id', PUT_cleanToggle())
 
-router.put('/cleanToggle/:id', PUT_cleanToggle())
+router.post("/addTag", POST_createTag());
+// router.post('/createTag', POST_createTag())
+
+router.get("/allTags", GET_allTags());
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,25 +1,34 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 // Routes
-const washRoute = require('./routes/washRoute');
-const earlyEmail = require('./routes/earlyEmail');
-const searchRoute = require('./routes/searchRoute');
-const statesRouter = require("./routes/statesRouter")
+const washRoute = require("./routes/washRoute");
+const earlyEmail = require("./routes/earlyEmail");
+const searchRoute = require("./routes/searchRoute");
+const statesRouter = require("./routes/statesRouter");
 
 const server = express();
 
 server.use(express.json());
 server.use(cors());
-server.use('/api/wash', washRoute);
-server.use('/early_email', earlyEmail);
+
+// Always starts with API
+// Interal or external
+// Resource / action
+
+server.use("/api/internal/washer", washRoute);
+// server.use("/api/wash", washRoute);
+server.use("/early_email", earlyEmail);
 // TODO: Early email ^ will be depracated after launch
-server.use('/api/search', searchRoute)
+server.use("/api/search", searchRoute);
 // TODO: Rough architecture, need to come back at somepoint and think this through beter
-server.use('/api/utility', statesRouter)
+server.use("/api/utility", statesRouter);
 
+// Interal Routes: Anything the client facing app wont need
+// ChowScout Search Routes: Anything directly related to the needs of the client
+// Utility/Resoucres: Resource simplified endpoints that can be used interally or client facing
 
-server.get('/', (req, res) => {
-	res.status(200).json({ message: 'Testing End Point' });
+server.get("/", (req, res) => {
+	res.status(200).json({ message: "Testing End Point" });
 });
 
 module.exports = server;
