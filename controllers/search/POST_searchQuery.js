0b870292e3df0bee0007971e6f3a38f4db43be12
MatthewVaccaro <matthew.tylor.vaccaro@gmail.com>
2021-05-19 14:47:03 -0500

    Draft: Search fully works but requires more testing & orginizatiion

--- controllers/search/POST_searchQuery.js
+++ controllers/search/POST_searchQuery.js
@@ -1,40 +1,35 @@
 const basicActions = require("../../models/basicModel");
 const helpers = require("../../utils/helpers");
+const timeHelper = require("../../utils/timeHelpers");
 const geolib = require("geolib");
 const axios = require("axios");
 
 function POST_searchQuery() {
 	return async (req, res, next) => {
 		try {
-			// TODO Lat & Lng should grab the data given in the request
-			const findLocation = {
+			const { latitude, longitude } = req.body;
+
+			const usersCoords = {
 				location: {
 					latLng: {
-						lat: 36.1254902176224,
-						lng: -86.78860731230652,
+						lat: latitude,
+						lng: longitude,
 					},
 				},
 			};
-			const searchQuery = "pizza";
+
 			// SECTION Finding and valdiating the location given
-			const locationDetails = await axios.post("http://www.mapquestapi.com/geocoding/v1/reverse?key=i8qSZ0ywgd3UA6JANlugttQlGJTNeeYm", findLocation);
-			const foundLocation = await locationDetails.data.results[0].locations[0].adminArea3.toLowerCase();
-			const validateLocation = await basicActions.findByAny("abbreviation", foundLocation, "states");
+			const findLocation = await axios.post("http://www.mapquestapi.com/geocoding/v1/reverse?key=i8qSZ0ywgd3UA6JANlugttQlGJTNeeYm", usersCoords);
+			const foundLocation = await findLocation.data.results[0].locations[0].adminArea3.toLowerCase();
+			const validateLocation = await basicActions.findWithFilter("abbreviation", foundLocation, "states");
 			helpers.checkLength(validateLocation, "location not found", res);
 
 			// SECTION Find all restaurants with the location REF ID
-			const findRestaurants = await basicActions.findAll("restaurants", {
-				state_ref: validateLocation[0].id,
-				washed: true,
-			});
+			const findRestaurants = await basicActions.findWithMultiFilter({ state_ref: validateLocation[0].id, washed: true }, "restaurants");
 
 			// SECTION Create all of the radius groups needed
-
-			const { latitude, longitude } = req.body;
 			const searchRadius = 1.609; // NOTE 1km = 1.609 miles
-
 			const searchableSections = [];
-
 			for (var i = 1; i < 11; i++) {
 				//SECTION for each loop we grab a set of bounds and cross the sections (below) to get the four points of our bounding box.
 				const findRadius = geolib.getBoundsOfDistance(
@@ -48,8 +43,6 @@ function POST_searchQuery() {
 				findRadius.push((searchRadius * i) / searchRadius);
 				searchableSections.push(findRadius);
 			}
-
-			console.log(searchableSections);
 
 			const restaurantsSortedByDistance = {};
 
@@ -75,17 +68,41 @@ function POST_searchQuery() {
 				}
 			}
 
-			console.log(restaurantsSortedByDistance);
+			// var fakeObject = {
+			// 	// Normal
+			// 	mon: "5:00 AM 11:00 AM",
+			// 	tue: "5:00 AM 11:00 AM",
+			// 	wed: "11:00 AM 10:00 PM",
+			// 	thu: "11:00 AM 8:00 PM",
+			// 	fri: "11:00 AM 8:00 PM",
+			// 	sat: "11:00 AM 3:00 PM",
+			// 	sun: "Closed",
+			// };
+
+			// console.log(timeHelper.isOpen(fakeObject));
 
 			const results = {};
+			// TODO: This double looping promise.all is most likely not scaleable. Needs stress tests
+			const promiseFinal = await Promise.all(
+				Object.keys(restaurantsSortedByDistance).map(async (mile) => {
+					return (fullRestaurant = await Promise.all(
+						restaurantsSortedByDistance[mile].map(async (restaurant) => {
+							const foundDishes = await basicActions.searchQuery(req.params.query, restaurant.id);
+							if (foundDishes.length > 0) {
+								const getHours = await basicActions.findWithFilter("restaurant_ref", restaurant.id, "hours");
+								restaurant.isOpen = timeHelper.isOpen(getHours[0]);
+								if (results[mile]) {
+									results[mile].push({ restaurant: restaurant, dishes: foundDishes });
+								} else {
+									results[mile] = [{ restaurant: restaurant, dishes: foundDishes }];
+								}
+							}
+						})
+					));
+				})
+			);
 
-			for (const mileSection in restaurantsSortedByDistance) {
-				const result = await restaurantsSortedByDistance[mileSection].map(async (restaurant, i) => {
-					const fullMenu = await basicActions.searchQuery(searchQuery, restaurant.id);
-					if (fullMenu.length !== 0) {
-					}
-				});
-			}
+			return res.status(200).json(results);
 		} catch (error) {
 			next(error);
 		}
---
+++ utils/timeHelpers.js
@@ -0,0 +1,64 @@
+const dateConstructor = new Date();
+const currentDate = dateConstructor.toString().split(" ");
+const day = dateConstructor.getDay(); // Returns INT 0 - 6
+
+const allDays = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
+
+const today = currentDate[0].toLowerCase();
+const yesterday = day === 0 ? allDays[6] : allDays[day - 1]; // On Sunday, yestorday equals saturday
+
+const time = currentDate[4].split(":");
+const hour = parseInt(time[0] + time[1]); //[0] Select Hour | [1] Selects Minute
+
+function isOpen(hoursObject) {
+	const restaurantHoursToday = hoursObject[today].split(" "); // ["5:00", "AM", "11:00", "AM"]
+	const restaurantHoursYesterday = hoursObject[yesterday].split(" "); // ["5:00", "AM", "11:00", "AM"]
+
+	//NOTE: If current time & closed time is AM
+	if (restaurantHoursYesterday[0] !== "Closed" && restaurantHoursYesterday[3] === "AM") {
+		const yesterdayOpenHour = restaurantHoursYesterday[0].split(":");
+		const yesterdayClosedHour = restaurantHoursYesterday[2].split(":");
+		// Final check to know if the AM time is extending the day - if Open time is > closed time (11am = 1100 > 1am = 100)
+		if (parseInt(yesterdayClosedHour[0] + yesterdayClosedHour[1]) < parseInt(yesterdayOpenHour[0] + yesterdayOpenHour[1])) {
+			if (hour < parseInt(yesterdayClosedHour[0] + yesterdayClosedHour[1])) {
+				return true;
+			}
+		}
+	}
+	//NOTE: Check if Closed
+	if (restaurantHoursToday[0] === "Closed") return false;
+
+	const restaurantOpenHours = restaurantHoursToday[0].split(":");
+	const restaurantCloseHours = restaurantHoursToday[2].split(":");
+
+	const openMeridian = restaurantHoursToday[1];
+	const closedMeridian = restaurantHoursToday[3];
+
+	const openHour =
+		openMeridian === "AM"
+			? parseInt(restaurantOpenHours[0] + restaurantOpenHours[1])
+			: parseInt(restaurantOpenHours[0] + restaurantOpenHours[1]) + 1200;
+
+	const closedHour =
+		closedMeridian === "AM"
+			? parseInt(restaurantCloseHours[0] + restaurantCloseHours[1])
+			: parseInt(restaurantCloseHours[0] + restaurantCloseHours[1]) + 1200;
+
+	console.log(`${today}, ${hour}, ${openHour}, ${closedHour} `);
+
+	//NOTE: Normal check: Current > open && Current < closed
+	if (hour > openHour && hour < closedHour) {
+		return true;
+	}
+
+	//NOTE: If current time is PM & closed time is AM
+	if (hour > openHour && restaurantHoursToday[3] === "AM") {
+		return true;
+	}
+
+	return false;
+}
+
+module.exports = {
+	isOpen,
+};
