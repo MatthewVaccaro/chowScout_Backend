a94594d0039471bde179a9ab9ca6115564d339bf
MatthewVaccaro <matthew.tylor.vaccaro@gmail.com>
2021-07-17 16:25:23 -0500

    updating knexfile

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const server = require('./server');
-require('dotenv').config();
+const server = require("./server");
+require("dotenv").config();
 
 const PORT = process.env.PORT || 4000;
 
 server.listen(PORT, () => {
-	console.log(`Listening on port ${PORT}...`);
+	console.log(`Listening on ${PORT === 4000 ? "Local Development:" : "production"} Port: ${PORT}...`);
 });
--- knexfile.js
+++ knexfile.js
@@ -1,48 +1,46 @@
-require('dotenv').config();
-const pg = require('pg');
+require("dotenv").config();
+const pg = require("pg");
 
 pg.defaults.ssl = {
 	rejectUnauthorized: false,
- }
+};
 
 module.exports = {
 	development: {
-		client: 'pg',
-        // eslint-disable-next-line no-undef
-        connection: process.env.DATABASE_URL,
-        ssl: {
-              rejectUnauthorized: false
-        },
+		client: "pg",
+		// eslint-disable-next-line no-undef
+		connection: process.env.DATABASE_URL,
+		ssl: {
+			rejectUnauthorized: false,
+		},
 		pool: {
 			min: 2,
-			max: 10
+			max: 10,
 		},
 		migrations: {
-			directory: './data/migrations'
+			directory: "./data/migrations",
 		},
-		seeds: { directory: './data/seeds' },
+		seeds: { directory: "./data/seeds" },
 		afterCreate: (conn, done) => {
 			// runs after a connection is made to the sqlite engine
-			conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
+			conn.run("PRAGMA foreign_keys = ON", done); // turn on FK enforcement
 		},
-		useNullAsDefault: true
+		useNullAsDefault: true,
 	},
 	production: {
-		client: 'pg',
-        // eslint-disable-next-line no-undef
-        connection: process.env.PROD_DATABASE_URL,
-        ssl: {
-              rejectUnauthorized: false
-        },
+		client: "pg",
+		// eslint-disable-next-line no-undef
+		connection: process.env.DATABASE_URL,
+		ssl: {
+			rejectUnauthorized: false,
+		},
 		pool: {
 			min: 2,
-			max: 10
+			max: 10,
 		},
 		migrations: {
-			directory: './data/migrations'
+			directory: "./data/migrations",
 		},
-		useNullAsDefault: true
+		useNullAsDefault: true,
 	},
-	
 };
-
